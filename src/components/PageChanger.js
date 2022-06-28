dfb1f8a9e5266c80a5c4ef86f883a53848579612
ksdev <kumar.kumardev12345@gmail.com>
2022-06-28 20:01:26 +0530

    initial

--- src/components/PageChanger.js
+++ src/components/PageChanger.js
@@ -1,6 +1,6 @@
 export default function PageChanger({ currPage, setCurrPage }) {
   return (
-    <div className="flex items-center justify-center space-x-5 fixed w-full py-5 border-t-2 bottom-0 left-0">
+    <div className="bg-gray-100 flex items-center justify-center space-x-5 fixed w-full py-5 border-t-2 bottom-0 left-0">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-6 w-6 cursor-pointer"
@@ -18,7 +18,9 @@ export default function PageChanger({ currPage, setCurrPage }) {
           d="M10 19l-7-7m0 0l7-7m-7 7h18"
         />
       </svg>
-      <div className="text-2xl border-2 px-2 select-none">{currPage}</div>
+      <div className="text-2xl border-2 bg-white rounded-full px-2 select-none">
+        {currPage}
+      </div>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-6 w-6 cursor-pointer"
