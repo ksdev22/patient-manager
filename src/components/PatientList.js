22fe80426937c8e4ad2516f7a8610f5a09b3da94
ksdev <kumar.kumardev12345@gmail.com>
2022-06-30 01:26:28 +0530

    initial

--- src/components/PatientList.js
+++ src/components/PatientList.js
@@ -14,70 +14,72 @@ export default function PatientList({
 }) {
   return (
     <div>
-      <div className="bg-white z-[95] grid grid-flow-col font-bold px-5 border-b-2 border-black select-none min-w-max sticky top-0 left-0 h-[10vh]">
-        {filters.columns.map((column) => {
-          if (column.checked) {
-            return (
-              <div
-                key={column.name}
-                className="w-40 border-l-2 pl-3 text-center flex items-center space-x-5"
-              >
-                <div>
-                  {column.name === "name" && "Name"}
-                  {column.name === "age" && "Age"}
-                  {column.name === "gender" && "Gender"}
-                  {column.name === "phone" && "Phone"}
-                  {column.name === "vaccinationStatus" && "Vaccination Status"}
-                  {column.name === "vaccineName" && "Vaccine Name"}
-                  {column.name === "symptoms" && "Symptoms"}
-                  {column.name === "medicalHistory" && "Medical History"}
-                </div>
-                <div
-                  className="cursor-pointer"
-                  onClick={() => {
-                    customSort(column.name, column.sortType);
-                    setFilters((prev) => {
-                      const newColumns = filters.columns.map((c) => {
-                        if (c.name === column.name) {
-                          if (column.sortType === "asc") {
-                            c.sortType = "dsc";
-                          } else {
-                            c.sortType = "asc";
-                          }
-                        }
-                        return c;
-                      });
-                      return { ...prev, columns: newColumns };
-                    });
-                  }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
-                    />
-                  </svg>
-                </div>
-              </div>
-            );
-          } else {
-            return null;
-          }
-        })}
-      </div>
       <ul
-        className="relative scrollbar scrollbar-thumb-gray-500 scrollbar-track-gray-300 scrollbar-thumb-rounded-full scrollbar-track-rounded-full overflow-scroll h-[60vh] pb-5"
+        className="relative scrollbar scrollbar-thumb-gray-500 scrollbar-track-gray-300 scrollbar-thumb-rounded-full scrollbar-track-rounded-full overflow-scroll h-[70vh] pb-5"
         // onMouseOver={(e) => patientListScroll()}
-        onMouseEnter={(e) => patientListScroll()}
+        onMouseOver={(e) => patientListScroll()}
       >
+        <div className="bg-white z-[95] grid grid-flow-col font-bold px-5 border-b-2 border-black select-none min-w-max sticky top-0 left-0">
+          {filters.columns.map((column) => {
+            if (column.checked) {
+              return (
+                <div
+                  key={column.name}
+                  className="w-40 border-l-2 pl-3 h-[10vh] text-center flex items-center justify-center space-x-5"
+                >
+                  <div>
+                    {column.name === "name" && "Name"}
+                    {column.name === "age" && "Age"}
+                    {column.name === "gender" && "Gender"}
+                    {column.name === "phone" && "Phone"}
+                    {column.name === "vaccinationStatus" &&
+                      "Vaccination Status"}
+                    {column.name === "vaccineName" && "Vaccine Name"}
+                    {column.name === "symptoms" && "Symptoms"}
+                    {column.name === "medicalHistory" && "Medical History"}
+                  </div>
+                  <div
+                    className="cursor-pointer"
+                    onClick={() => {
+                      customSort(column.name, column.sortType);
+                      setFilters((prev) => {
+                        const newColumns = filters.columns.map((c) => {
+                          if (c.name === column.name) {
+                            if (column.sortType === "asc") {
+                              c.sortType = "dsc";
+                            } else {
+                              c.sortType = "asc";
+                            }
+                          }
+                          return c;
+                        });
+                        return { ...prev, columns: newColumns };
+                      });
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+
         {patients.map((patient) => (
           <li
             key={patient.id}
